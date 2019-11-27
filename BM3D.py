a6aa14912fca3c5d76b8df00ebd4a5efc01c6087
AxelTchaikovsky <lzx1999sjtu@gmail.com>
2019-11-27 23:38:38 +0800

    forward

--- BM3D.py
+++ BM3D.py
@@ -2,4 +2,15 @@ import cv2
 import PSNR
 import numpy
 
-cv2.set 
+cv2.setUsePptimized(True)
+
+#parameters initialization
+sigma = 25
+Threshold_Hard3D = 2.7*sigma
+First_Match_threshold = 2500
+Step1_Max_matched_cnt = 16
+Step1_Blk_Size = 8
+Step1_Blk_Step = 3
+Step1_Search_Step = 3
+Step1_Search_Window = 39
+
